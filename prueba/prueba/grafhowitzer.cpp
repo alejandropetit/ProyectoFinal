9634d3da34c377228cadf6a80df414d5dde84fae
jose alejandro <alejandropetit11@gmail.com>
2022-04-09 15:59:57 -0500

    pruebas con 2 ramas
    prueba con una segunda rama

---
+++ prueba/prueba/grafhowitzer.cpp
@@ -0,0 +1,6 @@
+#include "grafhowitzer.h"
+
+grafHowitzer::grafHowitzer()
+{
+
+}
---
+++ prueba/prueba/grafhowitzer.h
@@ -0,0 +1,11 @@
+#ifndef GRAFHOWITZER_H
+#define GRAFHOWITZER_H
+
+
+class grafHowitzer
+{
+public:
+    grafHowitzer();
+};
+
+#endif // GRAFHOWITZER_H
